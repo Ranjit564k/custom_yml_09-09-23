019b94033a5d1d49f5c2a61d5250eba995c276a4
ranjeet <ranjitk692@gmail.com>
2024-01-04 20:30:28 +0530

    basic app

---
+++ app.py
@@ -0,0 +1,5 @@
+"""
+this is testing for custom yaml for ci
+"""
+def custom():
+    return "Hello"
---
+++ test.py
@@ -0,0 +1,7 @@
+from app import custom
+
+
+
+def test_fun():
+    assert custom() == "Hello" # this test is called unit testing
+
